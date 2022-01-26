ab9da0f65677a431b8254591e2982a32aae6b3e0
aticig <gulsum.atici@canonical.com>
2022-01-26 15:47:52 +0300

    minor-fixes

--- charms/kamailio-bundle/copy.sh
+++ charms/kamailio-bundle/copy.sh
@@ -2,7 +2,7 @@
 
 pushd charms/kamailio-bundle/
 path=`pwd`
-for i in kamailio sipp-k8s
+for i in kamailio-k8s sipp-k8s
 do
     cp  $i'-operator/'$i'.charm'  $path/../../kamailio_cnf/juju-bundles/$i'-operator/'
 done
