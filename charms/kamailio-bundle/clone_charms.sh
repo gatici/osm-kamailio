3fec8d55f519333a636bf8bd1c6384d2c34bfdad
aticig <gulsum.atici@canonical.com>
2022-01-26 13:06:47 +0300

    changed scripts

--- charms/kamailio-bundle/clone_charms.sh
+++ charms/kamailio-bundle/clone_charms.sh
@@ -2,7 +2,7 @@
 
 pushd charms/kamailio-bundle/
   git clone  -b main https://github.com/davigar15/charm-kamailio.git \
-  && cp -r charm-kamailio/* kamailio-operator/ && rm -rf charm-kamailio;
+  && cp -r charm-kamailio/* kamailio-k8s-operator/ && rm -rf charm-kamailio;
   git clone  https://github.com/davigar15/sipp-operator.git  \
   && cp -r sipp-operator/*  sipp-k8s-operator/ && rm -rf sipp-operator;
 
