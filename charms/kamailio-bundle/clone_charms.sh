0a9567a393c7d40c7d7af3bc6b7c9268cf95a109
Gulsum Atici <gulsumatici@gmail.com>
2022-01-26 12:48:32 +0300

    Update clone_charms.sh

--- charms/kamailio-bundle/clone_charms.sh
+++ charms/kamailio-bundle/clone_charms.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pushd charms/kamailio-bundle/
-  git clone  -b minor-fixes https://github.com/davigar15/charm-kamailio.git \
+  git clone  -b main https://github.com/davigar15/charm-kamailio.git \
   && cp -r charm-kamailio/* kamailio-operator/ && rm -rf charm-kamailio;
   git clone  https://github.com/davigar15/sipp-operator.git  \
   && cp -r sipp-operator/*  sipp-k8s-operator/ && rm -rf sipp-operator;
